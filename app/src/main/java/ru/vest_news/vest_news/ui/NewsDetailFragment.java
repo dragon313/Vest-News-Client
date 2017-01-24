744e0d136904ba2938734e29e8e867a8dcd916b8
AV <dragon313@mail.ru>
2017-01-24 09:10:01 +0500

    Добавил фрагмент для отображение детального описания новости.

--- app/src/main/java/ru/vest_news/vest_news/ui/NewsDetailFragment.java
+++ app/src/main/java/ru/vest_news/vest_news/ui/NewsDetailFragment.java
@@ -1,9 +1,31 @@
 package ru.vest_news.vest_news.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.vest_news.vest_news.R;
+
 
 public class NewsDetailFragment extends Fragment {
+
     public static NewsDetailFragment newInstance() {
         return new NewsDetailFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_news_detail, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
