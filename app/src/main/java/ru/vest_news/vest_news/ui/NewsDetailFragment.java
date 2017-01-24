70902d6e9254eb17005ea58d6a56666b52de9859
AV <dragon313@mail.ru>
2017-01-24 16:51:21 +0500

    Удалил файл с темами для apk v21.

--- app/src/main/java/ru/vest_news/vest_news/ui/NewsDetailFragment.java
+++ app/src/main/java/ru/vest_news/vest_news/ui/NewsDetailFragment.java
@@ -1,10 +1,21 @@
 package ru.vest_news.vest_news.ui;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class NewsDetailFragment extends Fragment{
     public static NewsDetailFragment newInstance() {
         return new NewsDetailFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
