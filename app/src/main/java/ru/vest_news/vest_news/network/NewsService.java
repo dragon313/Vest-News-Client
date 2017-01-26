59ef9465210c078aeb566a576c9469d2c0c45628
AV <dragon313@mail.ru>
2017-01-26 14:56:37 +0500

    Начало положено! Создал класс будущего сервиса.

---
+++ app/src/main/java/ru/vest_news/vest_news/network/NewsService.java
@@ -0,0 +1,22 @@
+package ru.vest_news.vest_news.network;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class NewsService extends IntentService {
+    private static final String TAG = "NewsService";
+    public NewsService(String name) {
+        super(TAG);
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, NewsService.class);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i(TAG, "Received an intent: " + intent);
+    }
+}
