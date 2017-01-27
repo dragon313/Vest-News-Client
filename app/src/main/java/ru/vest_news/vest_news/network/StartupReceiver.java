18f8e3e8c83dfab175d9ce88bc0e8eb4f5e93319
AV <dragon313@mail.ru>
2017-01-27 14:21:05 +0500

    Создал класс широковещательного приемника и добавил его в манифест.

---
+++ app/src/main/java/ru/vest_news/vest_news/network/StartupReceiver.java
@@ -0,0 +1,14 @@
+package ru.vest_news.vest_news.network;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Received broadcast intent: " + intent.getAction());
+    }
+}
